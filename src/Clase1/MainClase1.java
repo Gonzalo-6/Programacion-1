a3d4fdcccb5f519c413edfc58f5f6613e6216439
Gonzalo-6 <g-arranz@hotmail.com>
2025-03-21 14:37:43 +0100

    primera clase

---
+++ src/Clase1/MainClase1.java
@@ -0,0 +1,8 @@
+public class MainClase1 {
+        public static void main(String[] arg){
+   byte numeroByte = 127; //Número pequeño (-128 a 127)
+               System.out.println("byte (8 bits): " +numeroByte);
+
+        }
+
+}
